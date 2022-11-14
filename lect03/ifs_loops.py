a365b12f9bf0fc635f41c169ddbb4fea15f033a8
CCmentorMK <michal.kruczkowski@codecool.com>
2022-11-14 13:27:56 +0100

    slp lecture03

---
+++ lect03/ifs_loops.py
@@ -0,0 +1,31 @@
+matrix2D = [[2,3,4],[0,3,-1],[1,2,1]]
+
+# 2 3 4
+# 0 3 -1
+# 1 2 1
+#
+# for row in matrix2D:
+#     for value in row:
+#         if value < 0:
+#             exit()
+#         print(value, end=" ")
+#     print()
+
+
+# x = int(input("x="))
+# y = int(input("y="))
+# result = 1
+# while y:
+#     result *= x
+#     y -= 1
+# print(result)
+
+# CTRL + /
+# CTRL + SHIFT + /
+# CTRL + D
+
+val = ' '
+if(val):
+    print("is true")
+else:
+    print("is false")
