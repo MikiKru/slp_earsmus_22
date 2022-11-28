fb1b11f07ec4e7b6a719f4d70fae675fe6e20b9e
CCmentorMK <michal.kruczkowski@codecool.com>
2022-11-28 13:22:04 +0100

    lect04

--- lect04/functions.py
+++ lect04/functions.py
@@ -18,3 +18,31 @@ def student_grades(**studnet_grades):
 student_grades(name="Michal", grades=[3,4,5])
 student_grades(name = "Adam", grades1 = 3, grade2 = 4,grade3 = 5)
 student_grades(name = "John", grades1 = 3, grade2 = 4)
+
+global_var = 1
+
+# function that returns elements of geometric series
+def arithmetic_series(a, r, n):
+    elements = []
+    for iter in range(n):
+        elements.append(a*(r**iter))
+    return elements
+
+print(arithmetic_series(2,2,4))
+
+# function that prints different values -> black or white
+colour = "black"
+def get_colour():
+    global colour           # information that function uses global object
+    if colour == "black":
+        colour = "white"
+        return colour
+    else:
+        colour = "black"
+        return colour
+
+print(get_colour())
+print(get_colour())
+print(get_colour())
+print(get_colour())
+print(get_colour())
